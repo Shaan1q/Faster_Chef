f84f36277a83e64c345e5b50a7ef1e34b57728d1
competitioner <anon>
2026-04-27 13:39:23 -0400

    Initial Commit

---
+++ Faster_Chef/Faster_ChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  Faster_ChefApp.swift
+//  Faster_Chef
+//
+//  Created by Shaanzay Iqbal on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Faster_ChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
